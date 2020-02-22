793fede9274e19b44fae959169b039e44a04076f
magoer <maoyantsing@gmail.com>
2020-02-22 20:00:17 +0800

    Update 6vDownloader.user.js

--- 6vDownloader.user.js
+++ 6vDownloader.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         6vDownloader
 // @homepage     https://github.com/maoger/6vDownloader
-// @version      0.6
+// @version      0.7.200222
 // @description  [追剧专用]方便获取所有视频的下载链接，批量下载。
 // @author       Maoger
 // @match        http*://*.6vhao.tv/*
 // @match        http*://*.66s.cc/*
-// @require      http://code.jquery.com/jquery-3.2.1.js
+// @match        http*://*.6vgood.com/*
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @updateURL    https://openuserjs.org/meta/maoger/6vDownloader.meta.js
+// @copyright    2019-2020, maoger (https://openuserjs.org/users/maoger)
 // @license      MIT
 
 // ==/UserScript==
@@ -15,26 +17,28 @@
 (function() {
     'use strict';
 
-    // 定位
-    var $DingWei = $("body");
-
+    var $position = $("body");
     var $hr = $("<div/>")
         .html("<br /><hr /><br />");
 
-    // 新建：装载“ed2k类型”下载链接数据的容器
+    // ed2k
     var $ed2k_Container = $("<div/>")
-        .html("<br /><hr /><span style='font-size: 16px'>当前页面的所有迅雷下载链接，如下所示；<br />在<b style='color: #ff0000'>打开迅雷</b>的情况下，<b style='color: #ff0000'>复制</b>以下链接即可批量下载。<br /><br />[类型1]ed2k类型（电驴）下载链接：<hr style='height:1px;border:none;border-top:1px dashed #ADADAD;'/></span>");
+        .html("<br /><hr /><span style='font-size: 16px'>当前页面的所有迅雷下载链接，如下所示；<br />在<b style='color: #ff0000'>打开迅雷</b>的情况下，<b style='color: #ff0000'>复制</b>以下链接即可批量下载。<br /><br /><strong>类型1：</strong> ed2k类型（电驴）下载链接：<hr style='height:1px;border:none;border-top:1px dashed #ADADAD;'/></span>");
+    $ed2k_Container.insertAfter($position);
 
-    $ed2k_Container.insertAfter($DingWei);
+    // magnet
+    var $magnet_container = $("<div/>")
+        .html("<br /><hr /><span style='font-size: 16px'><strong>类型2：</strong> magnet类型（磁力）下载链接:<hr style='height:1px;border:none;border-top:1px dashed #ADADAD;'/></span>");
+    $magnet_container.insertAfter($ed2k_Container);
+    $hr.insertAfter($magnet_container);
 
-    // 新建：装载“magnet类型”下载链接数据的容器
-    var $magnet_Container = $("<div/>")
-        .html("<br /><hr /><span style='font-size: 16px'>[类型2]magnet类型（磁力）下载链接:<hr style='height:1px;border:none;border-top:1px dashed #ADADAD;'/></span>");
+    // thunder
+    var $thunder_container = $("<div/>")
+        .html("<br /><hr /><span style='font-size: 16px'><strong>类型3：</strong> thunder类型（迅雷）下载链接:<hr style='height:1px;border:none;border-top:1px dashed #ADADAD;'/></span>");
+    $thunder_container.insertAfter($magnet_container);
+    $hr.insertAfter($thunder_container);
 
-    $magnet_Container.insertAfter($ed2k_Container);
-    $hr.insertAfter($magnet_Container);
-
-    // 新建：存放进度条的容器
+    // 进度条
     var $loadContainer = $("<div/>")
         .addClass("barSpace")
         .css({
@@ -42,12 +46,7 @@
             "background" : "grey",
             "border-radius" : "1px",
         });
-
-    // 将进度条的容器插入 下载链接容器 和 首页“下载链接”标签 中间
-    $loadContainer.insertAfter($DingWei);
-
-
-    // 新建：进度条
+    $loadContainer.insertAfter($position);
     var $bar = $("<span/>")
         .attr("id" , "barsmooth" )
         .addClass("smooth")
@@ -56,22 +55,17 @@
             "width" : "0%",
             "border-radius" : "1px",
             "display" : "block",
-            "box-shadow" : "0px 0px 10px 1px #CC66FF",// 原来这里使用的是3B8CF8
+            "box-shadow" : "0px 0px 10px 1px #CC66FF",
             "background-color" : "#fff"
         });
-
-    // 插入：将“进度条”插入“进度条容器”中
     $loadContainer.append($bar);
 
-    // 定义：一个Number类型变量，用于设置进度条的width的百分比
     var m = 0;
 
-    // 加载 下载链接
     var c ='';
     var hrefArr = document.getElementsByTagName('a');
     var n = hrefArr.length;
     for( var i=0; i<hrefArr.length; i++ ){
-        // 设置进度条显示的百分比
         var loadingRate = self.setInterval(function (){
             if ( m < (i-1)*(200/n) ) {
                 m = (i-1)*(200/n);
@@ -92,7 +86,6 @@
 
         c = hrefArr[i].href;
 
-        // 构建包裹元素
         var $elt = $("<table/>")
             .addClass("tablebox")
             .css({
@@ -105,9 +98,11 @@
         }
         if (c.indexOf("magnet:?")>=0){
             $elt.html("<span>" + c + "</span>");
-            $magnet_Container.append($elt);
+            $magnet_container.append($elt);
         }
-
+        if (c.indexOf("thunder://")>=0){
+            $elt.html("<span>" + c + "</span>");
+            $thunder_container.append($elt);
+        }
     }
-
 })();
